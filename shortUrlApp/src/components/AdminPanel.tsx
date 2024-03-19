5d0868d9fad541dbf49c28acd7a5ce42d953e785
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-03-19 00:00:37 -0500

    add AdminPanel Component

---
+++ shortUrlApp/src/components/AdminPanel.tsx
@@ -0,0 +1,14 @@
+import { useAuth } from "../providers/AuthProvider";
+import AdminLayout from "../layouts/AdminLayout";
+import { FormShortUrl } from "./FormShortUrl";
+
+export default function AdminPanel() {
+  const auth = useAuth();
+
+  return (
+    <AdminLayout>
+        <h1>Bienvenido {auth.getAuthUser()?.name ?? ""}</h1>
+        <FormShortUrl />
+    </AdminLayout>
+  );
+}
