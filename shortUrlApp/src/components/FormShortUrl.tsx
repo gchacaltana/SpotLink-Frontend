15d5fa1d371dfe39a02b4057cf77efcea039713e
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-03-19 00:00:51 -0500

    add FormShortUrl Component

---
+++ shortUrlApp/src/components/FormShortUrl.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { validateURL } from "../utils/StringUtil";
+import { fetchShortLink } from "../api/LinkApi";
+import { useAuth } from "../providers/AuthProvider";
+import { LinkResponse } from "../types/types";
+
+export const FormShortUrl = () => {
+    const auth = useAuth();
+    const [url, setUrl] = useState("");
+    const [loading, setLoading] = useState("");
+    const [shortUrl, setShortUrl] = useState("");
+    const [error, setError] = useState("");
+
+    async function shortenURL() {
+        try {
+            setShortUrl("");
+            setError("");
+            setLoading("Acortando URL....");
+            if (!validateURL(url)) {
+                throw new Error("La URL ingresada no es válida");
+            }
+            
+            const accessToken = auth.getAccessToken();
+            const response = await fetchShortLink(url, accessToken);
+            if (!response.ok) {
+                const json = (await response.json()) as { message: string };
+                throw new Error(json.message);
+            }
+            setLoading("");
+            const json = (await response.json()) as LinkResponse;
+            if (json.token) {
+                setUrl("");
+                setShortUrl(`${window.location.origin}/${json.token}`);
+            }
+        } catch (error: any) {
+            setLoading("");
+            setError(error.message);
+        }
+    }
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        shortenURL();
+    }
+
+    function handleCopy(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        navigator.clipboard.writeText(shortUrl)
+            .then(() => {
+                console.log('Texto copiado al portapapeles');
+
+            })
+            .catch(err => {
+                console.error('Error al copiar el texto:', err);
+            });
+        setShortUrl("");
+    }
+
+    return (
+        <>
+            <h2>Acortador de URL</h2>
+            <div className="container-form">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        name="email"
+                        type="text"
+                        placeholder="Ingresa una URL para acortar"
+                        value={url}
+                        required={true}
+                        maxLength={1000}
+                        onChange={(e) => setUrl(e.target.value)}
+                    />
+                    <button type="submit">Cortar URL</button>
+                    {error && <div className="errorMessage">{error}</div>}
+                    {loading && <div className="loadingMessageFormShort">{loading}</div>}
+                    {shortUrl ? (
+                        <>
+                            <div className="successMessage">{shortUrl}</div>
+                            <button onClick={handleCopy}>Copiar</button>
+                        </>
+                    ) : ""}
+                </form>
+            </div>
+        </>
+    );
+}
