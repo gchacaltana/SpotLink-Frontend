6c48a56d96cbccc37ade3fd08c926305b3efa09e
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-03-18 23:57:45 -0500

    add routes folder

---
+++ shortUrlApp/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,7 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuth } from "../providers/AuthProvider";
+
+export default function ProtectedRoute() {
+  const auth = useAuth();
+  return auth.isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+}
