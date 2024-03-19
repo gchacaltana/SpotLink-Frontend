54ae1216a5fc5475a3c8d9a148625756d73b77ae
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-03-19 00:01:01 -0500

    add Profile Component

---
+++ shortUrlApp/src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import AdminLayout from "../layouts/AdminLayout";
+import { useAuth } from "../providers/AuthProvider";
+
+export default function Profile() {
+  const auth = useAuth();
+
+  return (
+    <AdminLayout>
+      <h1>Mi Perfil</h1>
+      <div className="container-profile">
+        <div className="user-info">
+          <div className="user-info-item">
+            <label>Email:</label>
+            <span>{auth.getAuthUser()?.email}</span>
+          </div>
+          <div className="user-info-item">
+            <label>Nombre:</label>
+            <span>{auth.getAuthUser()?.name}</span>
+          </div>
+          <div className="user-info-item">
+            <label>Fecha de creación:</label>
+            <span>{auth.getAuthUser()?.created_at}</span>
+          </div>
+        </div>
+      </div>
+    </AdminLayout>
+  );
+}
