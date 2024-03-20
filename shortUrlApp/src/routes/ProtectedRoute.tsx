039c945fdee2b2d0a4e3fee88a06d9f45a8756a6
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-03-20 06:39:42 -0500

    fix get auth context value in ProtectedRoute

--- shortUrlApp/src/routes/ProtectedRoute.tsx
+++ shortUrlApp/src/routes/ProtectedRoute.tsx
@@ -3,5 +3,5 @@ import { useAuth } from "../providers/AuthProvider";
 
 export default function ProtectedRoute() {
   const auth = useAuth();
-  return auth.isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+  return auth?.isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 }
